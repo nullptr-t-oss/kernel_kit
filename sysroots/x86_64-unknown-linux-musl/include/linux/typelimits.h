6831baa7380ea36eec7a0528c616d2af56eec2fe
nullptr_t <nullptr.oss@gmail.com>
2026-09-22 16:04:01 +0530

    Squashed 'bin/kernel/prebuilts/build-tools/' content from commit f4391ec
    git-subtree-dir: bin/kernel/prebuilts/build-tools
    git-subtree-split: f4391ecafd532f3577a1a9ca0297c4bc1e6e0538

---
+++ sysroots/x86_64-unknown-linux-musl/include/linux/typelimits.h
@@ -0,0 +1,11 @@
+/*
+ * This file is auto-generated. Modifications will be lost.
+ *
+ * See https://android.googlesource.com/platform/bionic/+/master/libc/kernel/
+ * for more information.
+ */
+#ifndef _UAPI_LINUX_TYPELIMITS_H
+#define _UAPI_LINUX_TYPELIMITS_H
+#define __KERNEL_INT_MAX ((int) (~0U >> 1))
+#define __KERNEL_INT_MIN (- __KERNEL_INT_MAX - 1)
+#endif
